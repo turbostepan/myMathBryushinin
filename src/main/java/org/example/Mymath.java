31c35b885fa19dabf4dff5a8b60be0e6047c3b6b
DaniilBryushinin <admin@510EC8.att.edu.ru>
2023-09-04 14:33:29 +0300

    const + minus

---
+++ src/main/java/org/example/Mymath.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Mymath {
+    public static final double PI = 3.1415926535;
+    public static final double E = 2.71828;
+
+
+public static void minus(double one) {
+    double two = one * -1;
+    System.out.println(two);
+}
+
+
+}
